968c209a989ef56e4b09f53dc7223bfd573a0548
Nick Bosch <nick@boschdesign.com.au>
2019-02-22 12:53:48 +1100

    Disable source maps in production builds

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  // If production JavaScript and CSS build
+  if (stage === 'build-javascript') {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    });
+  }
+};

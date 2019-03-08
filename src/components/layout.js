adb7d32bdce2e1d05f06e35e00cf13aaa8ef7543
Nick Bosch <nick@boschdesign.com.au>
2019-03-08 13:02:30 +1100

    Add dark theme, update logo, tweak particles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
 
-import { Global, css } from "@emotion/core";
+import { Global, css } from "@emotion/core"
 
 const globalStyles = css`
   body {
-    font-family: "Avenir Next", "Nunito", "Helvetica Neue", Helvetica, sans-serif;
+    font-family: "Avenir Next", "Nunito", "Helvetica Neue", Helvetica,
+      sans-serif;
     font-weight: 300;
-    color: rgb(10, 10, 10);
+    color: #fff;
+    background: hsl(218, 50%, 10%);
   }
-`;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -30,10 +32,10 @@ const Layout = ({ children }) => (
       </div>
     )}
   />
-);
+)
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
